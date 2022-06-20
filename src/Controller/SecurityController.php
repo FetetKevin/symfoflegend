120b9a8aa48cbe0ac3c7bb4f1e301a9590213442
Fetet Kevin <fetetkevin@gmail.com>
2022-06-20 13:14:16 +0200

    ajout d'un formulaire d'inscription

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="app_register")
+     */
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $encoder)
+    {  
+        $user = new User();
+        
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $clearPass = $user->getPassword();
+            $hash = $encoder->hashPassword($user, $clearPass);
+
+            $user->setPassword($hash);
+            $user->setRegisterDate(new \DateTime());
+
+            $manager->persist($user);
+            $manager->flush();
+
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'formRegistration' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ * fields = {"email"},
+ * message="L'adresse mail est déjà utilisée."
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min=4,
+     *      max=15,
+     *      minMessage="Votre pseudo doit contenir 4 caractères minimum.",
+     *      maxMessage="Votre pseudo ne doit contenir que 15 caractères maximum."
+     * )
+     */
+    private $nickname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="6", minMessage="Votre mot de passe doit contenir 6 caractères minimum.")
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Vos mots de passe doivent correspondre.")
+     */
+    public $confirm_password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $registerDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRegisterDate(): ?\DateTimeInterface
+    {
+        return $this->registerDate;
+    }
+
+    public function setRegisterDate(\DateTimeInterface $registerDate): self
+    {
+        $this->registerDate = $registerDate;
+
+        return $this;
+    }
+
+    public function eraseCredentials(){}
+
+    public function getUsername(){}
+
+    public function getSalt(){}
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+}
