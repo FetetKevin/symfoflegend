2fa9ffcc2332a9484d7e02333cf86fc6159182a0
Fetet Kevin <fetetkevin@gmail.com>
2022-06-27 15:49:00 +0200

    retrait du filtre raw sur l'affichage des commentaire pour eviter les failles xss

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,11 +9,9 @@ use App\Entity\Comment;
 
 class AppFixtures extends Fixture
 {
-
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create('fr_FR');
-
         $json_champs = file_get_contents('http://ddragon.leagueoflegends.com/cdn/12.8.1/data/fr_FR/champion.json');
         $champions = json_decode($json_champs);
         $champion = $champions->data;
@@ -28,24 +26,20 @@ class AppFixtures extends Fixture
                      ->setLore($v->blurb)
                      ->setImage($url_images)
                      ->setDifficulty($v->info->difficulty);
-
             $manager->persist($champion);
             
             for($i = 1; $i <= mt_rand(2, 5); $i++)
             {
                 $comment = new Comment();
-
                 $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
 
                 $comment->setAuthor($faker->name)
                         ->setContent($content)
                         ->setCreatedAt(new \DateTime())
                         ->setChampion($champion);
-
                 $manager->persist($comment);
             }
         }
-
         $manager->flush();
     }
 }
